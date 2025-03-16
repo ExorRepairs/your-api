7a0789c50fd5b82fd40d17d29f8842b36daefe31
ExorRepairs <exorrepairs@gmail.com>
2025-03-16 10:34:14 -0700

    Update server.js

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ app.use("/api/jobs", jobRoutes);
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-
+  console.log(`✅ Server running on port ${PORT}`);
+});
